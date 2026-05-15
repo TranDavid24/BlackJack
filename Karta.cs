a5fc54fff02a0524d7e58eda5e84cb18a2a4f78a
Matyáš Rut <adocz17@gmail.com>
2026-05-15 11:19:49 +0200

    Add: Logika a vlastnosti třídy Karta

---
+++ Karta.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Blackjack
+{
+    class Karta
+    {
+        public string Hodnota { get; private set; }
+        public string Barva { get; private set; }
+
+        public Karta(string hodnota, string barva)
+        {
+            Hodnota = hodnota;
+            Barva = barva;
+        }
+
+        public int GetCiselnouHodnotu()
+        {
+            if (Hodnota == "J" || Hodnota == "Q" || Hodnota == "K")
+                return 10;
+            if (Hodnota == "A")
+                return 11; 
+            return int.Parse(Hodnota);
+        }
+
+        public override string ToString()
+        {
+            return $"{Hodnota} {Barva}";
+        }
+    }
+}
