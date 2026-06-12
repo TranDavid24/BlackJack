d82f66a87d2716f9efb426a71e3562ec7f7d416b
Matyáš Rut <adocz17@gmail.com>
2026-06-12 10:29:18 +0200

    Update: Logika a překlad do češtiny

--- Hrac.cs
+++ Hrac.cs
@@ -1,24 +1,42 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Player
+namespace Blackjack
 {
-    public string Name { get; }
-    public List<int> Hand { get; }
-
-    public Player(string name)
+    public class Hrac
     {
-        Name = name;
-        Hand = new List<int>();
-    }
+        public string Jmeno { get; }
+        public List<Karta> Ruka { get; }
 
-    public void AddCard(int card)
-    {
-        Hand.Add(card);
-    }
+        public Hrac(string jmeno)
+        {
+            Jmeno = jmeno;
+            Ruka = new List<Karta>();
+        }
 
-    public int HandSum()
-    {
-        return Hand.Sum();
+        public void PridejKartu(Karta karta)
+        {
+            Ruka.Add(karta);
+        }
+
+        public int SpoctiSoucet()
+        {
+            int soucet = Ruka.Sum(karta => karta.GetCiselnouHodnotu());
+            int pocetEs = Ruka.Count(karta => karta.Hodnota == "A");
+
+            while (soucet > 21 && pocetEs > 0)
+            {
+                soucet -= 10;
+                pocetEs--;
+            }
+
+            return soucet;
+        }
+
+        public void VypisRuku()
+        {
+            Console.WriteLine($"{Jmeno} má karty: {string.Join(", ", Ruka)} (Součet: {SpoctiSoucet()})");
+        }
     }
 }
