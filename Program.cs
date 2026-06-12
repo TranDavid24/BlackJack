51c7b57cbbf14b513c43bf4e787479b605b279c6
Matyáš Rut <adocz17@gmail.com>
2026-06-12 10:02:41 +0200

    Add: hlavní program pro Blackjack

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace Blackjack
 {
@@ -6,11 +6,56 @@ namespace Blackjack
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.Title = "Blackjack";
+            Console.OutputEncoding = System.Text.Encoding.UTF8; 
 
-            Hra hra = new Hra();
-            hra.Spustit();
+            Console.Title = "♣ ♠ Blackjack ♥ ♦";
+            Console.WindowHeight = 35;
+            Console.WindowWidth = 80;
+
+            bool beziProgram = true;
+            string vyzva = "Co byste chtěli udělat?";
+            string[] moznostiMenu = new string[] { "(1) Hrát Blackjack", "(2) Zamíchat a ukázat balíček", "(3) Ukončit hru" };
+            int volbaMenu;
+
+            while (beziProgram)
+            {
+                Console.Clear();
+                NacteniVstupu.NactiVolbuMenu(vyzva, moznostiMenu, out volbaMenu);
+                
+                switch (volbaMenu)
+                {
+                    case 1:
+                        Console.Clear();
+                        Hra instanceHry = new Hra();
+                        bool hratZnovu = true;
+                        while (hratZnovu)
+                        {
+                            instanceHry.HrajKolo();
+                            int volba = NacteniVstupu.NactiCislo("\nChcete hrát znovu?\n(1) Ano   (2) Ne ", 1, 2);
+                            if (volba == 1)
+                            {
+                                Console.Clear();
+                                hratZnovu = true;
+                            }
+                            else break;
+                        }
+                        break;
+                        
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("--- ZAMÍCHANÝ BALÍČEK KARET ---");
+                        Balicek testovaciBalicek = new Balicek();
+                        testovaciBalicek.Zamichej();
+                        testovaciBalicek.VypisBalicek();
+                        Console.WriteLine("\nStiskněte libovolnou klávesu pro návrat do menu...");
+                        Console.ReadKey();
+                        break;
+                        
+                    case 3:
+                        beziProgram = false;
+                        break;
+                }
+            }
         }
     }
 }
