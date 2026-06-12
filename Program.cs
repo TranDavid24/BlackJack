18375463e48514e1f445fa03e629c182a4084d69
Matyáš Rut <adocz17@gmail.com>
2026-06-12 10:14:52 +0200

    Refactor: Překlad do češtiny

--- Program.cs
+++ Program.cs
@@ -1,55 +1,61 @@
-﻿using System;
+﻿﻿using System;
 
-namespace prblackjack
+namespace Blackjack
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8; // makes the symbols of cards show up
+            Console.OutputEncoding = System.Text.Encoding.UTF8; 
 
             Console.Title = "♣ ♠ Blackjack ♥ ♦";
-            Console.WindowHeight = 55;
-            Console.WindowWidth = 120;
+            Console.WindowHeight = 35;
+            Console.WindowWidth = 80;
 
-            bool runProgram = true;
-            string prompt = "What would you like to do? ";
-            string[] menuOptions = new string[] { "(1) Play Blackjack", "(2) Shuffle and Show Deck", "(3) Exit" };
-            int menuSelection;
-            while (runProgram)
+            bool beziProgram = true;
+            string vyzva = "Co byste chtěli udělat?";
+            string[] moznostiMenu = new string[] { "(1) Hrát Blackjack", "(2) Zamíchat a ukázat balíček", "(3) Ukončit hru" };
+            int volbaMenu;
+
+            while (beziProgram)
             {
                 Console.Clear();
-                ReadMethods.ReadChoice(prompt, menuOptions, out menuSelection);
-                switch (menuSelection)
+                NacteniVstupu.NactiVolbuMenu(vyzva, moznostiMenu, out volbaMenu);
+                
+                switch (volbaMenu)
                 {
-                    default:
+                    case 1:
                         Console.Clear();
-                        BlackjackGame gameInstance = new BlackjackGame();
-                        bool playBlackjack = true;
-                        int choice;
-                        while (playBlackjack)
+                        Hra instanceHry = new Hra();
+                        bool hratZnovu = true;
+                        while (hratZnovu)
                         {
-                            gameInstance.PlayRound();
-                            choice = ReadMethods.ReadInteger("Want to play again?\n(1) Yes   (2) No ", 1, 2);
-                            if (choice == 1)
-                                playBlackjack = true;
+                            instanceHry.HrajKolo();
+                            int volba = NacteniVstupu.NactiCislo("\nChcete hrát znovu?\n(1) Ano   (2) Ne ", 1, 2);
+                            if (volba == 1)
+                            {
+                                Console.Clear();
+                                hratZnovu = true;
+                            }
                             else break;
                         }
                         break;
+                        
                     case 2:
                         Console.Clear();
-                        Deck showdeck = new Deck(); // creates deck instance
-                        showdeck.DeckMethod(); // creates the deck
-                        showdeck.Shuffle(); // shuffles it
-                        showdeck.Print(0, 1); // prints it
+                        Console.WriteLine("--- ZAMÍCHANÝ BALÍČEK KARET ---");
+                        Balicek testovaciBalicek = new Balicek();
+                        testovaciBalicek.Zamichej();
+                        testovaciBalicek.VypisBalicek();
+                        Console.WriteLine("\nStiskněte libovolnou klávesu pro návrat do menu...");
                         Console.ReadKey();
                         break;
+                        
                     case 3:
-                        runProgram = false;
+                        beziProgram = false;
                         break;
-                } // menu options switch
-
-            } // runProgram while loop
-        } // main
-    } // program class
+                }
+            }
+        }
+    }
 }
